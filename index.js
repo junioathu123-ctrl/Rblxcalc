434ff972b4d1f71c065b4a5cd4d3231ebde02cf2
junioathu123-ctrl <junioathu123@gmail.com>
2026-05-12 00:13:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { 
+  Client, 
+  GatewayIntentBits,
+  EmbedBuilder
+} = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -14,7 +18,7 @@ const robuxBase = 149;
 
 const precoPorRobux = precoBase / robuxBase;
 
-client.on('messageCreate', (msg) => {
+client.on('messageCreate', async (msg) => {
   if (!msg.content.startsWith("$c")) return;
 
   let texto = msg.content.toLowerCase();
@@ -24,6 +28,7 @@ client.on('messageCreate', (msg) => {
 
   // pega número
   const match = texto.match(/[\d.]+/);
+
   if (!match) {
     return msg.reply("Use: `$c 500` ou `$c 20 reais`");
   }
@@ -44,6 +49,7 @@ client.on('messageCreate', (msg) => {
 
     resposta =
 `<:Dinheiros:1503157410372649030> R$${numero.toFixed(2).replace(".", ",")} = <:Bobux:1503157441859293305> ${Math.floor(robux)} Robux
+
 <:hm:1503185736185155615> Com taxa (só se for enviar pelo pls donate) = <:Bobux:1503157441859293305> ${Math.floor(robuxComTaxa)} Robux`;
 
   } 
@@ -60,11 +66,23 @@ client.on('messageCreate', (msg) => {
 
     resposta =
 `<:Bobux:1503157441859293305> ${numero} Robux = R$${reais.toFixed(2).replace(".", ",")}
+
 <:hm:1503185736185155615> Com taxa (só se for enviar pelo pls donate) = R$${reaisComTaxa.toFixed(2).replace(".", ",")}`;
 
   }
 
-  msg.reply(resposta);
+  const embed = new EmbedBuilder()
+    .setColor("#FFD700") // amarelo forte
+    .setTitle("🟡 Calculadora de Robux")
+    .setDescription(resposta)
+    .setFooter({
+      text: "Sistema de conversão"
+    })
+    .setTimestamp();
+
+  msg.reply({
+    embeds: [embed]
+  });
 });
 
 client.login(process.env.TOKEN);
