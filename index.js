89791baa1cdb819e594a6d8b8c84e5c321fc965f
junioathu123-ctrl <junioathu123@gmail.com>
2026-05-12 01:01:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,9 +48,9 @@ client.on('messageCreate', async (msg) => {
     const robuxComTaxa = robux / 0.7;
 
     resposta =
-`__***<:Dinheiros:1503157410372649030>  R$${numero.toFixed(2).replace(".", ",")} = <:Bobux:1503157441859293305> ${Math.floor(robux)} Robux***__
+`__**<:Dinheiros:1503157410372649030>  R$${numero.toFixed(2).replace(".", ",")} = <:Bobux:1503157441859293305> ${Math.floor(robux)} Robux**__
 
-__***<:hm:1503185736185155615>  Com taxa (só se for enviar pelo pls donate) = <:Bobux:1503157441859293305>  ${Math.floor(robuxComTaxa)} Robux***__`;
+__**<:hm:1503185736185155615>  Com taxa (só se for enviar pelo pls donate) = <:Bobux:1503157441859293305>  ${Math.floor(robuxComTaxa)} Robux**__`;
 
   } 
   
@@ -65,9 +65,9 @@ __***<:hm:1503185736185155615>  Com taxa (só se for enviar pelo pls donate) = <
     const reaisComTaxa = (numero / 0.7) * precoPorRobux;
 
     resposta =
-`__***<:Bobux:1503157441859293305> ${numero} Robux = R$${reais.toFixed(2).replace(".", ",")}***__
+`__**<:Bobux:1503157441859293305> ${numero} Robux = R$${reais.toFixed(2).replace(".", ",")}**__
 
-__***<:hm:1503185736185155615>  Com taxa (só se for enviar pelo pls donate) = *<:Dinheiros:1503157410372649030>  R$${reaisComTaxa.toFixed(2).replace(".", ",")}***__`;
+__**<:hm:1503185736185155615>  Com taxa (só se for enviar pelo pls donate) = *<:Dinheiros:1503157410372649030>  R$${reaisComTaxa.toFixed(2).replace(".", ",")}**__`;
 
   }
 
